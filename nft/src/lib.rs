c3d77a4ca40ce1e8f633e3ccaffdddaf06ea3562
cybor97 <muhametshin.1997@gmail.com>
2022-03-14 22:50:06 +0100

    Add nft_mint_purchase

--- nft/src/lib.rs
+++ nft/src/lib.rs
@@ -3,10 +3,10 @@ use near_contract_standards::non_fungible_token::metadata::{
 };
 use near_contract_standards::non_fungible_token::{NonFungibleToken, Token, TokenId};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::LazyOption;
+use near_sdk::collections::{LazyOption, UnorderedSet};
 use near_sdk::json_types::ValidAccountId;
 use near_sdk::{
-    env, near_bindgen, AccountId, BorshStorageKey, PanicOnDefault, Promise, PromiseOrValue,
+    env, near_bindgen, AccountId, Balance, BorshStorageKey, PanicOnDefault, Promise, PromiseOrValue,
 };
 
 near_sdk::setup_alloc!();
@@ -25,6 +25,7 @@ enum StorageKey {
     TokenMetadata,
     Enumeration,
     Approval,
+    TokensPerOwner { account_hash: Vec<u8> },
 }
 
 #[near_bindgen]
@@ -71,6 +72,73 @@ impl Contract {
         token_metadata: TokenMetadata,
     ) -> Token {
         self.tokens.mint(token_id, receiver_id, Some(token_metadata))
+    }
+
+    fn refund_deposit_to_account(storage_used: u64, account_id: AccountId) {
+        let required_cost = env::storage_byte_cost() * Balance::from(storage_used);
+        let attached_deposit = env::attached_deposit();
+        assert!(
+            required_cost <= attached_deposit,
+            "Must attach {} yoctoNEAR to cover storage",
+            required_cost,
+        );
+        let refund = attached_deposit - required_cost;
+        if refund > 1 {
+            Promise::new(account_id).transfer(refund);
+        }
+    }
+
+    #[payable]
+    pub fn nft_mint_purchase(
+        &mut self,
+        token_id: TokenId,
+        token_owner_id: AccountId,
+        token_metadata: Option<TokenMetadata>,
+        refund_id: Option<AccountId>,
+    ) -> Token {
+        let initial_storage_usage = refund_id.map(|account_id| (account_id, env::storage_usage()));
+
+        if token_metadata.is_none() {
+            env::panic("Must provide metadata".as_ref());
+        }
+        if self.tokens.owner_by_id.get(&token_id).is_some() {
+            env::panic("token_id must be unique".as_ref());
+        }
+        let attached_deposit = near_sdk::env::attached_deposit();
+
+        let owner_id: AccountId = token_owner_id;
+
+        self.tokens.owner_by_id.insert(&token_id, &owner_id);
+
+        self.tokens
+            .token_metadata_by_id
+            .as_mut()
+            .and_then(|by_id| by_id.insert(&token_id, token_metadata.as_ref().unwrap()));
+
+        if let Some(tokens_per_owner) = &mut self.tokens.tokens_per_owner {
+            let mut token_ids = tokens_per_owner.get(&owner_id).unwrap_or_else(|| {
+                UnorderedSet::new(StorageKey::TokensPerOwner {
+                    account_hash: env::sha256(owner_id.as_bytes()),
+                })
+            });
+            token_ids.insert(&token_id);
+            tokens_per_owner.insert(&owner_id, &token_ids);
+        }
+
+        let approved_account_ids =
+            if self.tokens.approvals_by_id.is_some() { Some(HashMap::new()) } else { None };
+
+        if let Some((id, storage_usage)) = initial_storage_usage {
+            assert!(
+                Balance::from(storage_usage) >= Balance::from(attached_deposit),
+                "Insufficient deposit attached to cover storage, should be at least {}",
+                storage_usage
+            );
+
+            Contract::refund_deposit_to_account(env::storage_usage() - storage_usage, id)
+        }
+
+        Token { token_id, owner_id, metadata: token_metadata, approved_account_ids }
     }
 }
 
